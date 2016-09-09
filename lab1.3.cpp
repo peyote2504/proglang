f06dd5f0178eb9c0483240820e9afe15c0eb1ee6
Nargiz <maratovna362@gmail.com>
2016-09-09 22:53:38 +0600

    Add files via upload

---
+++ lab1.3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int a,b;
+    cout<<"Enter two numbers"<<endl;
+    cin >>a>>b;
+    cout<<"The sum is="<<a+b<<endl;
+    cout<<"The difference is="<<a-b<<endl;
+    cout<<"The multipucle is="<<a*b<<endl;
+    cout<<"The divide is="<<a/b<<endl;
+   return 0;
+}
---
+++ lab1.4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{ cout << "What is your name? "<<endl;
+ string name;
+ cin>>name;
+ cout << "What is your surname? "<<endl;
+ string surname;
+ cin>>surname;
+ cout << "Hi " << name << " "<<surname<< "!\n";
+}
